5eae58a0195e3fd725be72c0e5f7a66dd1a9dda1
jhays <jhays@MacBook-Pro-de-John.local>
2020-11-02 08:28:55 -0800

    Initial commit with working Eureka server

---
+++ src/test/kotlin/com/imax/serviceregistry/ServiceRegistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.imax.serviceregistry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
